057fb7131aca74f7ebf5b78cc74cec739dbb4c01
hsma-chief-elf <d.chalk@exeter.ac.uk>
2023-05-16 14:43:30 +0100

    INIT: add my_conflict.py

---
+++ my_conflict.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 16 14:41:36 2023
+
+@author: dan
+"""
+
+def my_function(a, b):
+    result = a + b
+    
+    return result
+
