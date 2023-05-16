69a2c230206401904ff32f80a4d210f2efd08872
hsma-chief-elf <d.chalk@exeter.ac.uk>
2023-05-16 11:44:58 +0100

    INIT: add my_calc.py

---
+++ my_calc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 16 11:28:45 2023
+
+@author: dan
+"""
+
+def add_numbers(num_1, num_2):
+    result = num_1 + num_2
+    
+    return result
+
